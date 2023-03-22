e82e2b9b9a6eabfe6015ada3e3dc675cd493619d
Trever B <tjbruhn@wisc.edu>
2023-03-22 16:27:55 -0700

    add home widget and edit widget

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useRef, useEffect } from "react";
-import Bookmarks from "@arcgis/core/widgets/Bookmarks";
-import Expand from "@arcgis/core/widgets/Expand";
+
 import MapView from "@arcgis/core/views/MapView";
 import WebMap from "@arcgis/core/WebMap";
-
+import Bookmarks from "@arcgis/core/widgets/Bookmarks";
+import Expand from "@arcgis/core/widgets/Expand";
+import Home from "@arcgis/core/widgets/Home";
+import Editor from "@arcgis/core/widgets/Editor";
 import "./App.css";
 
 function App() {
@@ -16,14 +18,41 @@ function App() {
        */
       const webmap = new WebMap({
         portalItem: {
-          id: "6b985168463441f28942e228241e63c6", // template value: "aa1d3f80270146208328cf66d022e09c",
+          id: "6b985168463441f28942e228241e63c6",
         },
       });
 
       const view = new MapView({
         container: mapDiv.current,
         map: webmap,
+        center: [-123.085, 44.028],
+        zoom: 14,
+        constraints: {
+          minScale: 200000,
+          rotationEnabled: false,
+        },
       });
+
+      const homeWidget = new Home({
+        view: view,
+      });
+
+      // adds the home widget to the top left corner of the MapView
+      view.ui.add(homeWidget, "top-left");
+
+      const editor = new Editor({
+        view: view,
+        allowedWorkflows: "create-features",
+      });
+
+      const editExpand = new Expand({
+        view,
+        content: editor,
+        expanded: false,
+      });
+
+      // adds the edit widget to the top right corner of the MapView
+      view.ui.add(editExpand, "top-right");
 
       const bookmarks = new Bookmarks({
         view,
@@ -34,20 +63,11 @@ function App() {
       const bkExpand = new Expand({
         view,
         content: bookmarks,
-        expanded: true,
+        expanded: false,
       });
 
       // Add the widget to the top-right corner of the view
       view.ui.add(bkExpand, "top-right");
-
-      // bonus - how many bookmarks in the webmap?
-      webmap.when(() => {
-        if (webmap.bookmarks && webmap.bookmarks.length) {
-          console.log("Bookmarks: ", webmap.bookmarks.length);
-        } else {
-          console.log("No bookmarks in this webmap.");
-        }
-      });
     }
   }, []);
 
